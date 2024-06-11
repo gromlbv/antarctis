4334caa5772f3de570806634607cdde70368cc83
lbv_o <91362451+gromlbv@users.noreply.github.com>
2024-06-12 00:42:47 +0300

    upd + fonts fix

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("button").click(function(){
+      $(".fullscreen-absolute").fadeIn("300");
+    }) // Закрытие окошка
+    $(".fullscreen-absolute").click(function(){
+      $(".fullscreen-absolute").fadeOut("300");
+    })
+    $(".contents").click(function(e){
+      e.stopPropagation();
+    })
+  })
