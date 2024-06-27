b41227bd42cb697dcf309e23bb99a885fe8b6fdf
OPOLO <misha90638@gmail.com>
2024-06-27 23:25:49 +0300

    заполнение клиента

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ $(function(){
 })
 
 // цвет для select option "Выбрать"
-console.log($("#capacitor").text())
-if ($("#capacitor").text() == "Выбрать"){
-  console.log('aaa')
-}
+// console.log($("#capacitor").text())
+// if ($("#capacitor").text() == "Выбрать"){
+//   console.log('aaa')
+// }
 
 $(function(){
   $(".more-param-wr > button").click(function(){
@@ -33,3 +33,26 @@ $(function(){
       .animate({borderRadius: 18}, 50, "linear");
   })
 })
+
+
+const main = async () => {
+  let client = await fetch("/client")
+  client = await client.json()
+
+  document.querySelector(".client-name").innerText = client.name
+  document.querySelector(".client-corp").innerText = client.name
+
+  const raports = document.querySelector(".pdf-card-wr")
+
+  Object.values(client.raports).forEach(raport => {
+    raports.innerHTML += `
+                <div class="pdf-card">
+                    <div class="heading">
+                        <h3>${raport.name}</h3>
+                        <div class="date">${new Date(raport.date).toLocaleDateString("de-DE")}</div></div>
+                    <button class="pdf-btn"><img src="source/pdf.svg" draggable="false">Открыть PDF</button>
+                </div>`
+  });
+}
+
+main()
