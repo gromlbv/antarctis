352e5374bbd5863efb77a831b0ff0d8feda7afa1
OPOLO <106403765+opolonix@users.noreply.github.com>
2024-07-17 20:26:13 +0300

    дозагрузка формы после авторизации

--- profile.js
+++ profile.js
@@ -4,6 +4,23 @@ const main = async () => {
     let client = await fetch("/client")
     if (client.status == 401) window.location.href = "/login"
     client = await client.json()
+
+    form_to_send_fields = localStorage.getItem("form_to_send.fields")
+    form_to_send_name = localStorage.getItem("form_to_send.name")
+
+    if (form_to_send_name != null) {
+        result = await fetch(`/form/${form_to_send_name}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: form_to_send_fields
+        })
+
+        localStorage.removeItem("form_to_send.name")
+        localStorage.removeItem("form_to_send.fields")
+
+        let client = await fetch("/client")
+        client = await client.json()
+    }
   
     document.querySelector(".client-name").innerText = client.name
     document.querySelector(".client-corp").innerText = client.company_name
