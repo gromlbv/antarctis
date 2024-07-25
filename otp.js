4905da39304aea838c0f5f87435385bf7d88dff3
lbv_o <91362451+gromlbv@users.noreply.github.com>
2024-07-25 15:32:48 +0300

    изменения в js

---
+++ otp.js
@@ -0,0 +1,106 @@
+function nextDisable(){
+    $('.continue').attr('disabled', 'disabled');
+  }
+  function nextEnable(){
+    $('.continue').removeAttr('disabled');
+  }
+  nextDisable()
+  const form = document.querySelector("#otp-form");
+  const inputs = document.querySelectorAll(".otp-input");
+  const verifyBtn = document.querySelector("#verify-btn");
+  
+  const isAllInputFilled = () => {
+    return Array.from(inputs).every((item) => item.value);
+  };
+  
+  const getOtpText = () => {
+    let text = "";
+    inputs.forEach((input) => {
+      text += input.value;
+    });
+    return text;
+  };
+  const fields = document.querySelectorAll("input.otp-input")
+  const btn = document.querySelector("input.continue")
+  async function verifyFunc (e) {
+      code = ""
+      fields.forEach(e => {code += e.value})
+      result = await fetch(`/sms/verifyCode?code=${code}`)
+      if (result.status) {}
+      switch (result.status) {
+          case 401: window.location.href = "/login" // пользователь не прошел первичную авторизацию
+          case 400: window.location.reload(), noLog() // код не подошел
+          case 422: window.location.reload() // чтото не то с валидацией полей
+          case 200: window.location.href = "/profile" // код 200 значит авторизация успешна
+          case 404: alert("Нет связи с сервером")
+      }
+  }
+  function noLog(){
+    $(".otp-error").css("display", "flex")
+    $("$otp-input").value = "5";
+  }
+  const verifyOTP = () => {
+    if (isAllInputFilled()) {
+      verifyFunc()
+    }
+  };
+  
+  const toggleFilledClass = (field) => {
+    if (field.value) {
+      field.classList.add("filled");
+    } else {
+      field.classList.remove("filled");
+    }
+  };
+  
+  form.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    console.log({ target, value });
+    toggleFilledClass(target);
+    if (target.nextElementSibling) {
+      target.nextElementSibling.focus();
+    }
+    verifyOTP();
+  });
+  
+  inputs.forEach((input, currentIndex) => {
+    // fill check
+    toggleFilledClass(input);
+  
+    // paste event
+    input.addEventListener("paste", (e) => {
+      e.preventDefault();
+      const text = e.clipboardData.getData("text");
+      console.log(text);
+      inputs.forEach((item, index) => {
+        if (index >= currentIndex && text[index - currentIndex]) {
+          item.focus();
+          item.value = text[index - currentIndex] || "";
+          toggleFilledClass(item);
+          verifyOTP();
+        }
+      });
+    });
+  
+    // backspace event
+    input.addEventListener("keydown", (e) => {
+      if (e.keyCode === 8) {
+        e.preventDefault();
+        input.value = "";
+        // console.log(input.value);
+        toggleFilledClass(input);
+        if (input.previousElementSibling) {
+          input.previousElementSibling.focus();
+        }
+      } else {
+        if (input.value && input.nextElementSibling) {
+          input.nextElementSibling.focus();
+        }
+      }
+    });
+  });
+  
+  verifyBtn.addEventListener("click", () => {
+    verifyOTP();
+  });
--- send_form.js
+++ send_form.js
@@ -1,3 +1,17 @@
+function chiller_1(){
+    let name = "chiller_1"
+    let fields = {
+        input1: $('input[name*="1-chiller-1"]').val(),
+        input2: $('input[name*="2-chiller-1"]').val(),
+        input3: $('input[name*="3-chiller-1"]').val(),
+        input4: $('input[name*="4-chiller-1"]').val(),
+        input5: $('select[name*="5-chiller-1"]').val(),
+        input6: $('select[name*="6-chiller-1"]').val(),
+    }
+    console.log()
+    send_form(name, fields);
+}
+$(".chiller-1").on("click", chiller_1);
 const send_form = async (name, fields) => {
     // :name - economizer, conditioner, absorber, chiller-1, chiller-2
     // :fields - поля в словаре ключ: строка или инт
